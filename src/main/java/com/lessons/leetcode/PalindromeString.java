0fd0c6c3dbdbcbee5156cd7a35e18053bc77e2ee
Эсонов Жахонгир <Zhakhongir.Esonov@softline.com>
2023-03-02 23:14:28 +0300

    Add PalindromeString

---
+++ src/main/java/com/lessons/leetcode/PalindromeString.java
@@ -0,0 +1,21 @@
+package com.lessons.leetcode;
+
+public class PalindromeString {
+
+  public static boolean isPalindrome(String s) {
+    if (s == null) {
+      throw new IllegalStateException("Передали в параметр null значение!");
+    }
+    String stringOnlyLettersAndNumbers = s.replaceAll("[^A-Za-zА-Яа-я0-9]", "");
+    int lengthStringOnlyLettersAndNumbers = stringOnlyLettersAndNumbers.length();
+    for (int i = 0; i < (lengthStringOnlyLettersAndNumbers / 2); ++i) {
+      if (!String.valueOf(stringOnlyLettersAndNumbers.charAt(i)).equalsIgnoreCase(
+          String.valueOf(
+              stringOnlyLettersAndNumbers.charAt(lengthStringOnlyLettersAndNumbers - i - 1)))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+}
