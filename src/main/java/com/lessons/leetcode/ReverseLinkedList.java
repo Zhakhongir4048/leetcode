f0eed88f6eac621af3f9208431fc139b749e9d08
Эсонов Жахонгир <Zhakhongir.Esonov@softline.com>
2023-03-03 20:01:04 +0300

    Add ReverseLinkedList

---
+++ src/main/java/com/lessons/leetcode/ReverseLinkedList.java
@@ -0,0 +1,41 @@
+package com.lessons.leetcode;
+
+public class ReverseLinkedList {
+
+  static class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+
+    @Override
+    public String toString() {
+      return "ListNode{" +
+          "val=" + val +
+          ", next=" + next +
+          '}';
+    }
+  }
+
+  public static ListNode reverseList(ListNode node) {
+    ListNode prev = null;
+    ListNode current = node;
+    ListNode next;
+    while (current != null) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+    node = prev;
+    return node;
+  }
+
+}
