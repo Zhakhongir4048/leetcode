db0888604ca37080c0522639a480ae8ee65597ed
Эсонов Жахонгир <dzhoha4048@mal.ru>
2023-07-26 13:02:49 +0300

    https://leetcode.com/problems/group-anagrams/description/ accepted

---
+++ src/main/java/com/lessons/leetcode/GroupAnagrams.java
@@ -0,0 +1,29 @@
+package com.lessons.leetcode;
+
+import java.util.*;
+
+/**
+ * <a href="https://leetcode.com/problems/group-anagrams/">...</a>
+ * timer 00 : 03 : 37
+ */
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            map.get(sortedWord).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new GroupAnagrams().groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+    }
+
+}
