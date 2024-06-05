f609617b8a3f1a3107fca3744b26b76ad772730e
Эсонов Жахонгир <dzhoha4048@mal.ru>
2024-06-05 21:05:54 +0300

    Новые задачи

--- README.md
+++ README.md
@@ -11,3 +11,4 @@
 9) https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 10) https://leetcode.com/problems/two-sum/description/
 11) https://leetcode.com/problems/length-of-last-word/description/
+12) https://leetcode.com/problems/plus-one/description/
---
+++ src/main/java/com/lessons/leetcode/array/PlusOne66.java
@@ -0,0 +1,30 @@
+package com.lessons.leetcode.array;
+
+import java.math.BigInteger;
+
+public class PlusOne66 {
+
+    public int[] plusOne(int[] digits) {
+        int[] result;
+        BigInteger sum = BigInteger.valueOf(0);
+        BigInteger ten = BigInteger.valueOf(10);
+
+        for (int k : digits) {
+            // (sum * 10) + (k % 10) - таким образом достаём первую цифру
+            sum = (sum).multiply(ten).add(BigInteger.valueOf(k % 10));
+        }
+
+        // Прибавили 1
+        sum = sum.add(BigInteger.valueOf(1));
+
+        char[] charArray = String.valueOf(sum).toCharArray();
+        result = new int[charArray.length];
+
+        for (int i = 0; i < charArray.length; i++) {
+            // Обязательно нужно делать - '0', потому, что будет конвертация в цифру, а не цифру из Unicode
+            result[i] = charArray[i] - '0';
+        }
+
+        return result;
+    }
+}
